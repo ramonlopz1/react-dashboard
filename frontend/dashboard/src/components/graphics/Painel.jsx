a052ad7807db8e8a43304c1760bb628bf04edbc3
ramonlopz <davidramon123@hotmail.com>
2022-06-30 10:25:12 -0300

    filter by year and Painel Grid

--- frontend/dashboard/src/components/graphics/Painel.jsx
+++ frontend/dashboard/src/components/graphics/Painel.jsx
@@ -3,7 +3,22 @@ import './Painel.css'
 
 const Painel = props =>
     <section className='content_children painel'>
-        <h1>Jan</h1>
+        <div className='painel__btns'>
+            <div className="painel__btns__container">
+                <button className="btn">Jan</button>
+                <button className="btn">Fev</button>
+                <button className="btn">Mar</button>
+                <button className="btn">Abr</button>
+                <button className="btn">Mai</button>
+                <button className="btn">Jun</button>
+                <button className="btn">Jul</button>
+                <button className="btn">Ago</button>
+                <button className="btn">Set</button>
+                <button className="btn">Out</button>
+                <button className="btn">Nov</button>
+                <button className="btn">Dez</button>
+            </div>
+        </div>
         <div className='container'>
             <div className="boxes">1</div>
             <div className="boxes">2</div>
