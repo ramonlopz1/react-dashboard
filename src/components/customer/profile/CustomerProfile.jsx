a172050b8f3e19af4f8eb77abb1dac03ddfdda28
ramonlopz <davidramon123@hotmail.com>
2022-07-13 14:40:59 -0300

    starting customer profile

--- src/components/customer/profile/CustomerProfile.jsx
+++ src/components/customer/profile/CustomerProfile.jsx
@@ -5,6 +5,8 @@ import { useParams } from 'react-router-dom'
 
 export default function CustomerProfile(props) {
 
+    // criar um fetch que obtenha o cliente através do code vindo no params.
+
     let { code } = useParams()
     console.log(code)
 
