8ac0424f2def79f1e2bc60839d11916f5148fcf6
ramonlopz <davidramon123@hotmail.com>
2022-07-22 08:46:12 -0300

    colors

--- src/components/login/container-center/WelcomeContainer.jsx
+++ src/components/login/container-center/WelcomeContainer.jsx
@@ -1,10 +1,11 @@
 import styles from './WelcomeContainer.module.css'
-
+import img from '../../../assets/imgs/login__welcome/welcome_image.png'
 export default function WelcomeContainer() {
 
     return (
         <div className={styles.container_welcome}>
             <h1>Olá!</h1>
+            <img src={img} alt="welcome" className={styles.img_welcome}/>
             <p>Seja bem vindo, aqui você tem em mãos uma ferramenta que vai te proporcionar uma melhor análise, tomada de decisão e ciência dos dados da sua empresa.</p>
 
             <ul>
