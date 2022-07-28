c888be8f9df96932afe8013af40a801fd935f54f
ramonlopz <davidramon123@hotmail.com>
2022-07-28 08:28:58 -0300

    homepage charts bar

--- src/components/home/box-components/third-box/comparative-graphic/ComparativeGraphic.jsx
+++ src/components/home/box-components/third-box/comparative-graphic/ComparativeGraphic.jsx
@@ -1,28 +1,55 @@
 import styles from './ComparativeGraphic.module.css'
+import utils from '../../../../graphics/util/utils.js'
 import { Chart, registerables } from 'chart.js';
 import { Line } from 'react-chartjs-2'
+import { useEffect, useState } from 'react';
+
 Chart.register(...registerables);
 
 export default function ComparativeGraphics(props) {
 
+    const [currentYear, setCurrentYear] = useState([])
+    const [previousYear, setPreviousYear] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+            const data = await fetch('http://localhost:3000/revenue')
+            const datajson = await data.json()
+
+            const currYear = utils.getUpdateList(datajson, 2021)
+            const prevYear = utils.getUpdateList(datajson, 2020)
+
+            setCurrentYear(currYear)
+            setPreviousYear(prevYear)
+        }
+
+        getData()
+    }, [])
+
+   const months = [
+        'Jan', 'Fev', 'Mar', 'Abr', 'Mai',
+        'Jun', 'Jul', 'Ago', 'Set', 'Out',
+        'Nov', 'Dez'
+    ]
+
     const data = {
-        labels: ['a', 'b', 'c'],
+        labels: [...months],
         datasets: [{
-            label: 'first data',
+            label: 'Ano atual',
+            data: [...currentYear],
             borderColor: 'rgb(255, 99, 132)',
             backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            data: [12, 3, 4]
         },
         {
-            label: 'second data',
-            data: [5, 6, 8],
+            label: 'Ano passado',
+            data: [...previousYear],
             borderColor: 'rgb(53, 162, 235)',
             backgroundColor: 'rgba(53, 162, 235, 0.5)',
         }]
     }
 
     return (
-
+        
         <div className={styles.ComparativeGraphics}>
             <Line
                 data={data}
@@ -32,7 +59,7 @@ export default function ComparativeGraphics(props) {
                         mode: 'nearest',
                         axis: 'x',
                         intersect: false
-                      }
+                    }
                 }}
             />
         </div>
