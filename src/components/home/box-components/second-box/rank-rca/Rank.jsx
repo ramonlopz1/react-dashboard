5c77f05ee18f4ecfb3faaf9ee7b35f53d1b742bf
ramonlopz1 <rmnprojetos@outlook.com>
2022-07-28 21:29:48 -0300

    sort by desc on top 5 rank - home section

--- src/components/home/box-components/second-box/rank-rca/Rank.jsx
+++ src/components/home/box-components/second-box/rank-rca/Rank.jsx
@@ -1,6 +1,36 @@
 import styles from './Rank.module.css'
+import utils from '../../../../graphics/util/utils.js'
+import { useState, useEffect } from 'react'
 
 export default function Rank(props) {
+
+    const [state, setState] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/rca')
+            .then(data => data.json()).then(data => bestRevenues(data))
+
+    }, [])
+
+    const bestRevenues = (data) => {
+        
+        const totalList = {}
+        const dataEntries = Object.entries(data)
+
+        dataEntries.forEach(rca => {
+            const name = rca[0]
+            const rcaData = rca[1]?.revenue
+            
+            const totalRevenue = utils.calcYearRevenue(rcaData, 2021)
+            totalList[name] = totalRevenue
+        })
+        
+        const rankRevenue = Object.fromEntries(
+            Object.entries(totalList).sort(([,a],[,b]) => b-a)
+        );
+        console.log(rankRevenue)
+    }
+
     return (
         <div className={styles.wrap__table}>
             <table className={styles.rank__rca}>
